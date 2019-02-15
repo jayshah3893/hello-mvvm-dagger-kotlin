1f699308cdc4bd7cad4b1d24138fd08321aa793d
Paijo <paijorx1985@gmail.com>
2019-02-15 10:04:44 +0700

    Rename static database name to constant

--- app/src/main/java/com/paijorx/hello_mvvm_dagger2_kotlin/di/module/DatabaseModule.kt
+++ app/src/main/java/com/paijorx/hello_mvvm_dagger2_kotlin/di/module/DatabaseModule.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.room.Room
 import com.paijorx.hello_mvvm_dagger2_kotlin.dao.CryptoCurrencyDao
 import com.paijorx.hello_mvvm_dagger2_kotlin.db.Database
+import com.paijorx.hello_mvvm_dagger2_kotlin.utils.Constants
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -14,7 +15,7 @@ class DatabaseModule {
     @Provides
     @Singleton
     fun providesDatabase(application: Application): Database = Room.databaseBuilder(application,
-        Database::class.java,"CryptoCurrency.db").build()
+        Database::class.java, Constants.DATABASE_NAME).build()
 
     @Provides
     @Singleton
